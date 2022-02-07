e2b4e1823e98e3f45979307774f7d0c2891f475f
jmather625 <jatinm2@illinois.edu>
2022-02-07 02:02:46 -0600

    initial optical flow gets 0.66168

--- submission/evaluate.py
+++ submission/evaluate.py
@@ -3,6 +3,16 @@ import torch
 
 from climatehack import BaseEvaluator
 from model import Model
+import cv2
+
+
+def warp_flow(img, flow):
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:, :, 0] += np.arange(w)
+    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
+    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    return res
 
 
 class Evaluator(BaseEvaluator):
@@ -29,22 +39,37 @@ class Evaluator(BaseEvaluator):
         assert coordinates.shape == (2, 128, 128)
         assert data.shape == (12, 128, 128)
 
-        prediction = np.expand_dims(data[-1][32:96,32:96], 0)
-        prediction = np.tile(prediction, (24, 1, 1))
+        prev = data[-2]
+        cur = data[-1]
+        flow = cv2.calcOpticalFlowFarneback(
+            prev=prev,
+            next=cur,
+            flow=None,
+            pyr_scale=0.5,
+            levels=3,
+            winsize=15,
+            iterations=10,
+            poly_n=5,
+            poly_sigma=1.2,
+            flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN,
+        )
+
+        prediction = np.zeros((24, 64, 64), dtype=np.float32)
+        cutoff = 6
+        for j in range(1, 25):
+            yhat = warp_flow(cur, flow * j)
+            if j <= cutoff:
+                # if the multiplier is not too bad, do this:
+                # apply the flow `i` iterations
+                yhat = warp_flow(cur, flow * j)[32 : 32 + 64, 32 : 32 + 64]
+            else:
+                # otherwise, stick with the last optical flow prediction
+                yhat = cur[32 : 32 + 64, 32 : 32 + 64]  # prediction[cutoff - 2]
+
+            prediction[j - 2, :, :] = yhat
+
         assert prediction.shape == (24, 64, 64)
         return prediction
-
-        # with torch.no_grad():
-        #     prediction = (
-        #         self.model(torch.from_numpy(data).view(-1, 12 * 128 * 128))
-        #         .view(24, 64, 64)
-        #         .detach()
-        #         .numpy()
-        #     )
-
-        #     assert prediction.shape == (24, 64, 64)
-
-        #     return prediction
 
 
 def main():
---
+++ submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eEou pipefail
+
+python doxa_cli.py agent upload climatehack ./submission
