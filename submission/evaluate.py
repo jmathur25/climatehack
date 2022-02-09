c518c0ed61f7f5c4c450d241d7209e76ad5f0428
jmather625 <jatinm2@illinois.edu>
2022-02-09 03:28:09 -0600

    dumb simple RNN got 0.6538. trained on colab using 11 days of data

--- submission/evaluate.py
+++ submission/evaluate.py
@@ -2,17 +2,40 @@ import numpy as np
 import torch
 
 from climatehack import BaseEvaluator
-from model import Model
+import models
 import cv2
+import torch
 
 
-def warp_flow(img, flow):
-    h, w = flow.shape[:2]
-    flow = -flow
-    flow[:, :, 0] += np.arange(w)
-    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
-    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
-    return res
+MEAN = 209.0
+STD = 60.0
+DEVICE = torch.device("cpu")
+
+
+def _predict(model, srcs):
+    batch_size = srcs.shape[0]
+
+    # tensor to store decoder outputs
+    outputs = torch.zeros(batch_size, 24, 64, 64).to(DEVICE)
+
+    # last hidden state of the encoder is used as the initial hidden state of the decoder
+    hidden, cell = model.encoder(srcs)
+
+    # first input to the decoder is the last image in the hour
+    # shape: (batch_size, 1, 64, 64)
+    input = srcs[:, -1, 32:96, 32:96]
+    input = torch.unsqueeze(input, 1)
+
+    for t in range(24):
+        output, hidden, cell = model.decoder(input, hidden, cell)
+        # images of shape (batch_size, 64, 64)
+        output = output.view(batch_size, 64, 64)
+        outputs[:, t, :, :] = output
+
+        input = output
+        input = torch.unsqueeze(input, 1)
+
+    return outputs
 
 
 class Evaluator(BaseEvaluator):
@@ -20,10 +43,24 @@ class Evaluator(BaseEvaluator):
         """Sets up anything required for evaluation.
 
         In this case, it loads the trained model (in evaluation mode)."""
-        pass
-        # self.model = Model()
-        # self.model.load_state_dict(torch.load("model.pt"))
-        # self.model.eval()
+
+        INPUT_DIM = 128 * 128
+        OUTPUT_DIM = 64 * 64
+        ENC_EMB_DIM = 256
+        DEC_EMB_DIM = 256
+        HID_DIM = 512
+        N_LAYERS = 2
+        ENC_DROPOUT = 0.5
+        DEC_DROPOUT = 0.5
+
+        enc = models.Encoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT)
+        dec = models.Decoder(OUTPUT_DIM, DEC_EMB_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT)
+
+        self.model = models.Seq2Seq(enc, dec, DEVICE).to(DEVICE)
+        self.model.load_state_dict(
+            torch.load("simple_rnn_rand_15.pt", map_location=DEVICE)
+        )
+        self.model.eval()
 
     def predict(self, coordinates: np.ndarray, data: np.ndarray) -> np.ndarray:
         """Makes a prediction for the next two hours of satellite imagery.
@@ -39,34 +76,10 @@ class Evaluator(BaseEvaluator):
         assert coordinates.shape == (2, 128, 128)
         assert data.shape == (12, 128, 128)
 
-        prev = data[-2]
-        cur = data[-1]
-        flow = cv2.calcOpticalFlowFarneback(
-            prev=prev,
-            next=cur,
-            flow=None,
-            pyr_scale=0.5,
-            levels=3,
-            winsize=15,
-            iterations=10,
-            poly_n=5,
-            poly_sigma=1.2,
-            flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN,
-        )
-
-        prediction = np.zeros((24, 64, 64), dtype=np.float32)
-        cutoff = 6
-        for j in range(1, 25):
-            yhat = warp_flow(cur, flow * j)
-            if j <= cutoff:
-                # if the multiplier is not too bad, do this:
-                # apply the flow `i` iterations
-                yhat = warp_flow(cur, flow * j)[32 : 32 + 64, 32 : 32 + 64]
-            else:
-                # otherwise, stick with the last optical flow prediction
-                yhat = cur[32 : 32 + 64, 32 : 32 + 64]  # prediction[cutoff - 2]
-
-            prediction[j - 2, :, :] = yhat
+        data = torch.unsqueeze(torch.FloatTensor((data - MEAN) / STD), 0)
+        prediction = _predict(self.model, data)
+        prediction = prediction * STD + MEAN
+        prediction = prediction[0].detach().numpy()  # only 1 batch
 
         assert prediction.shape == (24, 64, 64)
         return prediction
