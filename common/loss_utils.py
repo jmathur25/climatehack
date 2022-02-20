3aca32c005bf4ac23fc87d8fba853ece21723bf3
Jatin Mathur <jatinm2@illinois.edu>
2022-02-20 15:42:10 -0600

    implemented a forecaster for arbitrary timesteps

--- common/loss_utils.py
+++ common/loss_utils.py
@@ -6,16 +6,15 @@ from pytorch_msssim import MS_SSIM
 class MS_SSIMLoss(nn.Module):
     """Multi-Scale SSIM Loss"""
 
-    def __init__(self, channels=1, **kwargs):
+    def __init__(self, data_range, channels, **kwargs):
         """
         Initialize
         Args:
-            convert_range: Convert input from -1,1 to 0,1 range
             **kwargs: Kwargs to pass through to MS_SSIM
         """
         super(MS_SSIMLoss, self).__init__()
         self.ssim_module = MS_SSIM(
-            data_range=1023.0, size_average=True, win_size=3, channel=channels, **kwargs
+            data_range=data_range, size_average=True, win_size=3, channel=channels, **kwargs
         )
 
     def forward(self, x: torch.Tensor, y: torch.Tensor):
