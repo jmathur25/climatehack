a2811de22d27a00f18df06d250a9750145d3bdd6
Jatin Mathur <jatinm2@illinois.edu>
2022-03-20 03:50:48 -0500

    make DCT training work. trains DGMR 2x faster but no improvement. also started tracking visualization script

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,7 @@
 import pyproj
+from scipy import fftpack
+import numpy as np
+import torch
 
 # OSGB is also called "OSGB 1936 / British National Grid -- United
 # Kingdom Ordnance Survey".  OSGB is used in many UK electricity
@@ -57,3 +60,141 @@ def osgb_to_lat_lon(x, y):
     Return: 2-tuple of latitude (north-south), longitude (east-west).
     """
     return transformers.osgb_to_lat_lon.transform(x, y)
+
+
+def get_idct_filter():
+    """
+    Returns a callable `idct_filter` which undoes the DCT-as-feature maps operation.
+
+    dct_maps: (batch_size, 64, 16, 16)
+
+    64 because we did an 8x8 DCT, so 64 coefficients
+    16x16 because the original image is of size 128, and we split
+    it into 8x8 blocks, so we have 16x16 of these blocks
+
+    """
+    e = fftpack.dct(np.eye(8), norm="ortho")
+    # for any 2D matrix `i`, you can get the 2D DCT with
+    # p = fftpack.dctn(i, norm='ortho')
+    # this is equal to:
+    # p == e.T @ i @ e
+
+    # we use these to undo the DCT
+    ei = np.linalg.inv(e)
+    eti = ei.T
+    # you can recover the original image by doing:
+    # ih = eti @ p @ ei
+    # np.allclose(ih, i)
+
+    eti_t = torch.FloatTensor(eti)
+    ei_t = torch.FloatTensor(ei)
+
+    def idct_filter(dct_maps):
+        nonlocal eti_t, ei_t
+        dct_maps = dct_maps.permute(0, 2, 3, 1)
+        b, blocks_h, blocks_w, f = dct_maps.shape
+        # turn flattened 64 -> 8x8
+        dct_maps = dct_maps.reshape(b, blocks_h, blocks_w, 8, 8)
+
+        eti_t = eti_t.to(dct_maps.device)
+        ei_t = ei_t.to(dct_maps.device)
+
+        # this undoes DCT on the 8x8 frequency coefficients
+        pixel_maps = eti_t @ dct_maps @ ei_t
+
+        # now, we reorganize on the width to make it contiguous
+        # dct_maps will not have all the width pixels represented contiguously
+        pixel_maps = pixel_maps.permute(0, 1, 3, 2, 4)
+        pixel_maps = pixel_maps.reshape(b, blocks_h, 8, blocks_w * 8)
+        # notice the height is also stored contiguously, we just need to reshape to get it
+        pixel_img = pixel_maps.reshape(b, blocks_h * 8 * blocks_w * 8).reshape(
+            -1, blocks_h * 8, blocks_w * 8
+        )
+        return pixel_img
+
+    return idct_filter
+
+
+def _create_dct_filter(k1, k2, n):
+    """
+    Basically straight from wikipedia: https://en.wikipedia.org/wiki/Discrete_cosine_transform.
+    Normalization is applied to keep the transform orthogonal. This should be identical
+    to what scipy.fftpack is doing, it just gets the weights for a specific (k1, k2) for
+    convolution.
+    """
+    f = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            f[i, j] = np.cos(np.pi / n * (i + 1 / 2) * k1) * np.cos(
+                np.pi / n * (j + 1 / 2) * k2
+            )
+            # apply normalization
+            if k1 == 0 and k2 == 0:
+                f[i, j] *= 1.0 / n
+            elif k1 == 0 or k2 == 0:
+                f[i, j] *= np.sqrt(2) / n
+            else:
+                f[i, j] *= 2 / n
+    return f
+
+
+def create_conv_dct_filter():
+    """
+    Create conv filters that work on a single channel image img. Make sure that:
+    img.shape == (1, h, w) such that h and w are divisble by 8
+
+    This convolution layer outputs 64 feature maps of size h//8, w//8. Accessing
+    index (i,j,k) will get the ith 8x8 DCT coefficient (flattened) for the (j,w)
+    block. You can think of a block as an 8x8 portion of an image that is non-overlapping
+    with other blocks. So (i,0,0) gets the ith DCT coefficient in the top-left block.
+    (i,2,4) gets the ith DCT coefficient on img[2*8:2*8+8,4*8+4*8+8].
+
+    The point of this is to have 64 feature maps that represent the image in the frequency domain.
+    Each feature map contains the same DCT coefficient, just on a different 8x8 block in the image.
+    """
+    n = 8
+    conv = torch.nn.Conv2d(1, n * n, kernel_size=(n, n), stride=n)
+    for i in range(n * n):
+        idxi = i // n
+        idxj = i - n * idxi
+        f = _create_dct_filter(idxi, idxj, n)
+        conv.weight.data[i, 0] = torch.FloatTensor(f)
+    # no bias
+    conv.bias.data = torch.zeros_like(conv.bias.data)
+    # turn off gradient calculation
+    conv.weight.requires_grad = False
+    conv.bias.requires_grad = False
+    return conv
+
+
+if __name__ == "__main__":
+    # run a test to ensure we are doing DCT correctly
+    bs = 3
+    x = np.random.randn(bs, 128, 128)
+    conv_dct = create_conv_dct_filter()
+    conv_idct = get_idct_filter()
+
+    x = torch.FloatTensor(x)
+    # add an input channel
+    x = torch.unsqueeze(x, dim=1)
+
+    dct_maps = conv_dct(x)
+    blocks_h = x.shape[2] // 8
+    blocks_w = x.shape[3] // 8
+    for b in range(bs):
+        for i in range(blocks_h):
+            for j in range(blocks_w):
+                # check every block for the correct DCT coefficients
+                block = x[b, 0, i * 8 : i * 8 + 8, j * 8 : j * 8 + 8].numpy()
+                conv_output = dct_maps[b, :, i, j].reshape(8, 8)
+                correct_output = fftpack.dctn(block, norm="ortho")
+                assert np.allclose(conv_output.numpy(), correct_output, atol=1e-6)
+
+    xhat = conv_idct(dct_maps)
+
+    xhat = np.squeeze(xhat.numpy())
+    x = np.squeeze(x.numpy())
+
+    assert np.allclose(x, xhat, atol=1e-6)
+
+    print("Success!")
---
+++ dgmr-freq-mod/dgmr/discriminators.py
@@ -0,0 +1,230 @@
+import torch
+from torch.nn.modules.pixelshuffle import PixelUnshuffle
+from torch.nn.utils.parametrizations import spectral_norm
+import torch.nn.functional as F
+from dgmr.common import DBlock
+from huggingface_hub import PyTorchModelHubMixin
+
+
+class Discriminator(torch.nn.Module, PyTorchModelHubMixin):
+    def __init__(
+        self,
+        input_channels: int = 12,
+        num_spatial_frames: int = 8,
+        conv_type: str = "standard",
+        **kwargs
+    ):
+        super().__init__()
+        config = locals()
+        config.pop("__class__")
+        config.pop("self")
+        self.config = kwargs.get("config", config)
+        input_channels = self.config["input_channels"]
+        num_spatial_frames = self.config["num_spatial_frames"]
+        conv_type = self.config["conv_type"]
+
+        self.spatial_discriminator = SpatialDiscriminator(
+            input_channels=input_channels, num_timesteps=num_spatial_frames, conv_type=conv_type
+        )
+        self.temporal_discriminator = TemporalDiscriminator(
+            input_channels=input_channels, conv_type=conv_type
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        spatial_loss = self.spatial_discriminator(x)
+        temporal_loss = self.temporal_discriminator(x)
+
+        return torch.cat([spatial_loss, temporal_loss], dim=1)
+
+
+class TemporalDiscriminator(torch.nn.Module, PyTorchModelHubMixin):
+    def __init__(
+        self, input_channels: int = 12, num_layers: int = 3, conv_type: str = "standard", **kwargs
+    ):
+        """
+        Temporal Discriminator from the Skillful Nowcasting, see https://arxiv.org/pdf/2104.00954.pdf
+
+        Args:
+            input_channels: Number of channels per timestep
+            crop_size: Size of the crop, in the paper half the width of the input images
+            num_layers: Number of intermediate DBlock layers to use
+            conv_type: Type of 2d convolutions to use, see satflow/models/utils.py for options
+        """
+        super().__init__()
+        config = locals()
+        config.pop("__class__")
+        config.pop("self")
+        self.config = kwargs.get("config", config)
+        input_channels = self.config["input_channels"]
+        num_layers = self.config["num_layers"]
+        conv_type = self.config["conv_type"]
+
+        self.downsample = torch.nn.AvgPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        self.space2depth = PixelUnshuffle(downscale_factor=2)
+        internal_chn = 48
+        self.d1 = DBlock(
+            input_channels=4 * input_channels,
+            output_channels=internal_chn * input_channels,
+            conv_type="3d",
+            first_relu=False,
+        )
+        self.d2 = DBlock(
+            input_channels=internal_chn * input_channels,
+            output_channels=2 * internal_chn * input_channels,
+            conv_type="3d",
+        )
+        self.intermediate_dblocks = torch.nn.ModuleList()
+        for _ in range(num_layers):
+            internal_chn *= 2
+            self.intermediate_dblocks.append(
+                DBlock(
+                    input_channels=internal_chn * input_channels,
+                    output_channels=2 * internal_chn * input_channels,
+                    conv_type=conv_type,
+                )
+            )
+
+        self.d_last = DBlock(
+            input_channels=2 * internal_chn * input_channels,
+            output_channels=2 * internal_chn * input_channels,
+            keep_same_output=True,
+            conv_type=conv_type,
+        )
+
+        self.fc = spectral_norm(torch.nn.Linear(2 * internal_chn * input_channels, 1))
+        self.relu = torch.nn.ReLU()
+        self.bn = torch.nn.BatchNorm1d(2 * internal_chn * input_channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.downsample(x)
+
+        x = self.space2depth(x)
+        # Have to move time and channels
+        x = torch.permute(x, dims=(0, 2, 1, 3, 4))
+        # 2 residual 3D blocks to halve resolution if image, double number of channels and reduce
+        # number of time steps
+        x = self.d1(x)
+        x = self.d2(x)
+        # Convert back to T x C x H x W
+        x = torch.permute(x, dims=(0, 2, 1, 3, 4))
+        # Per Timestep part now, same as spatial discriminator
+        representations = []
+        for idx in range(x.size(1)):
+            # Intermediate DBlocks
+            # Three residual D Blocks to halve the resolution of the image and double
+            # the number of channels.
+            rep = x[:, idx, :, :, :]
+            for d in self.intermediate_dblocks:
+                rep = d(rep)
+            # One more D Block without downsampling or increase number of channels
+            rep = self.d_last(rep)
+
+            rep = torch.sum(F.relu(rep), dim=[2, 3])
+            rep = self.bn(rep)
+            rep = self.fc(rep)
+
+            representations.append(rep)
+        # The representations are summed together before the ReLU
+        x = torch.stack(representations, dim=1)
+        # Should be [Batch, N, 1]
+        x = torch.sum(x, keepdim=True, dim=1)
+        return x
+
+
+class SpatialDiscriminator(torch.nn.Module, PyTorchModelHubMixin):
+    def __init__(
+        self,
+        input_channels: int = 12,
+        num_timesteps: int = 8,
+        num_layers: int = 4,
+        conv_type: str = "standard",
+        **kwargs
+    ):
+        """
+        Spatial discriminator from Skillful Nowcasting, see https://arxiv.org/pdf/2104.00954.pdf
+
+        Args:
+            input_channels: Number of input channels per timestep
+            num_timesteps: Number of timesteps to use, in the paper 8/18 timesteps were chosen
+            num_layers: Number of intermediate DBlock layers to use
+            conv_type: Type of 2d convolutions to use, see satflow/models/utils.py for options
+        """
+        super().__init__()
+        config = locals()
+        config.pop("__class__")
+        config.pop("self")
+        self.config = kwargs.get("config", config)
+        input_channels = self.config["input_channels"]
+        num_timesteps = self.config["num_timesteps"]
+        num_layers = self.config["num_layers"]
+        conv_type = self.config["conv_type"]
+        # Randomly, uniformly, select 8 timesteps to do this on from the input
+        self.num_timesteps = num_timesteps
+        # First step is mean pooling 2x2 to reduce input by half
+        self.mean_pool = torch.nn.AvgPool2d(2)
+        self.space2depth = PixelUnshuffle(downscale_factor=2)
+        internal_chn = 24
+        self.d1 = DBlock(
+            input_channels=4 * input_channels,
+            output_channels=2 * internal_chn * input_channels,
+            first_relu=False,
+            conv_type=conv_type,
+        )
+        self.intermediate_dblocks = torch.nn.ModuleList()
+        for _ in range(num_layers):
+            internal_chn *= 2
+            self.intermediate_dblocks.append(
+                DBlock(
+                    input_channels=internal_chn * input_channels,
+                    output_channels=2 * internal_chn * input_channels,
+                    conv_type=conv_type,
+                )
+            )
+        self.d6 = DBlock(
+            input_channels=2 * internal_chn * input_channels,
+            output_channels=2 * internal_chn * input_channels,
+            keep_same_output=True,
+            conv_type=conv_type,
+        )
+
+        # Spectrally normalized linear layer for binary classification
+        self.fc = spectral_norm(torch.nn.Linear(2 * internal_chn * input_channels, 1))
+        self.relu = torch.nn.ReLU()
+        self.bn = torch.nn.BatchNorm1d(2 * internal_chn * input_channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x should be the chosen 8 or so
+        idxs = torch.randint(low=0, high=x.size()[1], size=(self.num_timesteps,))
+        representations = []
+        for idx in idxs:
+            rep = self.mean_pool(x[:, idx, :, :, :])  # 128x128
+            rep = self.space2depth(rep)  # 64x64x4
+            rep = self.d1(rep)  # 32x32
+            # Intermediate DBlocks
+            for d in self.intermediate_dblocks:
+                rep = d(rep)
+            rep = self.d6(rep)  # 2x2
+            rep = torch.sum(F.relu(rep), dim=[2, 3])
+            rep = self.bn(rep)
+            rep = self.fc(rep)
+            """
+            Pseudocode from DeepMind
+            # Sum-pool the representations and feed to spectrally normalized lin. layer.
+            y = tf.reduce_sum(tf.nn.relu(y), axis=[1, 2])
+            y = layers.BatchNorm(calc_sigma=False)(y)
+            output_layer = layers.Linear(output_size=1)
+            output = output_layer(y)
+
+            # Take the sum across the t samples. Note: we apply the ReLU to
+            # (1 - score_real) and (1 + score_generated) in the loss.
+            output = tf.reshape(output, [b, n, 1])
+            output = tf.reduce_sum(output, keepdims=True, axis=1)
+            return output
+            """
+            representations.append(rep)
+
+        # The representations are summed together before the ReLU
+        x = torch.stack(representations, dim=1)
+        # Should be [Batch, N, 1]
+        x = torch.sum(x, keepdim=True, dim=1)
+        return x
